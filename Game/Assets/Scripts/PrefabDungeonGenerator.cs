276cdab230b97c69af930391942babb363d626ca
yoshi-isle <joshuajaworowski+1@gmail.com>
2025-08-25 23:51:18 -0400

    WIP

--- Game/Assets/Scripts/PrefabDungeonGenerator.cs
+++ Game/Assets/Scripts/PrefabDungeonGenerator.cs
@@ -8,6 +8,8 @@ public class PrefabDungeonGenerator : MonoBehaviour
     public GameObject playerPrefab;
     public GameObject deadEndPrefab;
 
+    public List<GameObject> rooms = new List<GameObject>();
+
     [SerializeField]
     public List<RoomWeight> roomChanceWeights = new List<RoomWeight>();
 
@@ -22,19 +24,33 @@ public class PrefabDungeonGenerator : MonoBehaviour
     {
         //var maxWeight = roomChanceWeights.Sum(rw => rw.weight);
         var initialRoom = Instantiate(roomChanceWeights.First().room, Vector3.zero, Quaternion.identity);
+        rooms.Add(initialRoom);
         Instantiate(playerPrefab, new Vector3(0, 10, 0), Quaternion.identity);
         BranchRoomOut(initialRoom);
+
+        foreach (var item in rooms)
+        {
+            var connectors = item.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
+            foreach (var c in connectors)
+            {
+                print($"Found unprocessed door: {c.name}");
+            }
+        }
     }
 
     void BranchRoomOut(GameObject room)
     {
-        var connectors = room.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door")).ToArray();
+        var connectors = room.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
         foreach (var item in connectors)
         {
             var newRoom = Instantiate(roomChanceWeights.First().room, Vector3.zero, Quaternion.identity);
-            var secondConnectors = newRoom.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door")).ToArray();
+            var secondConnectors = newRoom.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
             var newRoomRandomSecondConnector = secondConnectors[Random.Range(0, secondConnectors.Length)];
+            rooms.Add(newRoom);
             print($"Picked a random connector! {newRoomRandomSecondConnector.transform.position}");
+
+            item.tag = "Processed";
+            newRoomRandomSecondConnector.tag = "Processed";
 
             // Align the new room's connector with the original room's connector
             Quaternion targetRotation = item.rotation * Quaternion.Euler(0, 180f, 0);
