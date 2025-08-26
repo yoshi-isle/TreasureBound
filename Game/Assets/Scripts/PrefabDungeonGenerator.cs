5d2e1803657f4aab9ec3a8ee106312d2460591a7
Tangy <joshua.jaworowski@gmail.com>
2025-08-26 18:44:41 -0400

    Improved dungeon generation - collision checks!

--- Game/Assets/Scripts/PrefabDungeonGenerator.cs
+++ Game/Assets/Scripts/PrefabDungeonGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -22,42 +23,96 @@ public class PrefabDungeonGenerator : MonoBehaviour
 
     void Start()
     {
-        //var maxWeight = roomChanceWeights.Sum(rw => rw.weight);
-        var initialRoom = Instantiate(roomChanceWeights.First().room, Vector3.zero, Quaternion.identity);
-        rooms.Add(initialRoom);
-        Instantiate(playerPrefab, new Vector3(0, 10, 0), Quaternion.identity);
-        BranchRoomOut(initialRoom);
-
-        foreach (var item in rooms)
-        {
-            var connectors = item.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
-            foreach (var c in connectors)
-            {
-                print($"Found unprocessed door: {c.name}");
-            }
-        }
+        StartCoroutine(GenerateDungeonCoroutine());
     }
 
-    void BranchRoomOut(GameObject room)
+    public IEnumerator GenerateDungeonCoroutine()
     {
+        var initialRoom = Instantiate(GetRandomRoom(), Vector3.zero, Quaternion.identity);
+        rooms.Add(initialRoom);
+        Instantiate(playerPrefab, new Vector3(0, 10, 0), Quaternion.identity);
+        
+        yield return StartCoroutine(BranchRoomOutCoroutine(initialRoom));
+    }
+
+    GameObject GetRandomRoom()
+    {
+        var totalWeight = roomChanceWeights.Sum(rw => rw.weight);
+        var randomValue = Random.Range(0f, totalWeight);
+        
+        float currentWeight = 0f;
+        foreach (var roomWeight in roomChanceWeights)
+        {
+            currentWeight += roomWeight.weight;
+            if (randomValue <= currentWeight)
+            {
+                return roomWeight.room;
+            }
+        }
+        return roomChanceWeights.First().room;
+    }
+
+    IEnumerator BranchRoomOutCoroutine(GameObject room)
+    {
+        maxRooms--;
+        if (maxRooms <= 0)
+        {
+            yield break;
+        }
+        
         var connectors = room.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
         foreach (var item in connectors)
         {
-            var newRoom = Instantiate(roomChanceWeights.First().room, Vector3.zero, Quaternion.identity);
-            var secondConnectors = newRoom.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
-            var newRoomRandomSecondConnector = secondConnectors[Random.Range(0, secondConnectors.Length)];
-            rooms.Add(newRoom);
-            print($"Picked a random connector! {newRoomRandomSecondConnector.transform.position}");
+            var newRoom = Instantiate(GetRandomRoom(), Vector3.zero, Quaternion.identity);
+            var newRoomConnectors = newRoom.GetComponentsInChildren<Transform>().Where(x => x.name.Contains("Door") && x.tag == "Untagged").ToArray();
+            var newRoomRandomConnector = newRoomConnectors[Random.Range(0, newRoomConnectors.Length)];
+            
+            print($"Picked a random connector! {newRoomRandomConnector.transform.position}");
 
-            item.tag = "Processed";
-            newRoomRandomSecondConnector.tag = "Processed";
-
-            // Align the new room's connector with the original room's connector
             Quaternion targetRotation = item.rotation * Quaternion.Euler(0, 180f, 0);
-            newRoom.transform.rotation = targetRotation * Quaternion.Inverse(newRoomRandomSecondConnector.localRotation);
+            newRoom.transform.rotation = targetRotation * Quaternion.Inverse(newRoomRandomConnector.localRotation);
 
-            Vector3 offset = item.position - newRoomRandomSecondConnector.position;
+            Vector3 offset = item.position - newRoomRandomConnector.position;
             newRoom.transform.position += offset;
+            
+            print($"Placing new room at position: {newRoom.transform.position}");
+            print($"Connector positions - existing: {item.position}, new: {newRoomRandomConnector.position}, offset: {offset}");
+
+            if (HasRoomCollision(newRoom))
+            {
+                print("Collision detected! Skipping this connector...");
+                Destroy(newRoom);
+                continue;
+            }
+            
+            rooms.Add(newRoom);
+            
+            item.tag = "Processed";
+            newRoomRandomConnector.tag = "Processed";
+            
+            yield return new WaitForSeconds(0.1f);
+            
+            yield return StartCoroutine(BranchRoomOutCoroutine(newRoom));
         }
+    }
+
+    bool HasRoomCollision(GameObject newRoom)
+    {
+        var newRoomPosition = newRoom.transform.position;
+        var minDistance = 15f;
+        
+        foreach (var existingRoom in rooms)
+        {
+            var distance = Vector3.Distance(newRoomPosition, existingRoom.transform.position);
+            print($"Distance between rooms: {distance}");
+            
+            if (distance < minDistance)
+            {
+                print($"Rooms too close! Distance: {distance}, minimum required: {minDistance}");
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
