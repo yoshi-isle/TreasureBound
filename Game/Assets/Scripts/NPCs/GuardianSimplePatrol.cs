485d84592b9757c851212910ac6adc3f4289d22d
yoshi-isle <joshuajaworowski+1@gmail.com>
2025-08-29 01:31:20 -0400

    Refactor GuardianSimplePatrol to implement state management for patrol behavior

--- Game/Assets/Scripts/NPCs/GuardianSimplePatrol.cs
+++ Game/Assets/Scripts/NPCs/GuardianSimplePatrol.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class GuardianSimplePatrol : MonoBehaviour
 {
     List<Transform> patrolPoints;
     Transform currentPatrolPointTarget;
     CharacterController characterController;
+
+    PatrolStates currentState = PatrolStates.Patrolling;
+
+    enum PatrolStates
+    {
+        Patrolling,
+        PlayerSpotted,
+        Suspicious
+    }
     private int currentIndex = 0;
     void Start()
     {
@@ -31,17 +41,44 @@ public class GuardianSimplePatrol : MonoBehaviour
 
     void Update()
     {
+        switch (currentState)
+        {
+            case PatrolStates.Patrolling:
+                HandlePatrolUpdate();
+                break;
+            case PatrolStates.PlayerSpotted:
+                HandlePlayerSpotted();
+                break;
+            case PatrolStates.Suspicious:
+                HandleSuspicious();
+                break;
+        }
+
+    }
+
+    private void HandleSuspicious()
+    {
+        print("Method not yet implemented.");
+    }
+
+    private void HandlePlayerSpotted()
+    {
+        print("Method not yet implemented.");
+    }
+
+    private void HandlePatrolUpdate()
+    {
         if (currentPatrolPointTarget != null)
         {
-            var manhattenDistance = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(currentPatrolPointTarget.position.x, 0, currentPatrolPointTarget.position.z));
-            if (manhattenDistance < 1f)
+            var manhattanDistance = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(currentPatrolPointTarget.position.x, 0, currentPatrolPointTarget.position.z));
+            if (manhattanDistance < 1f)
             {
                 currentIndex = (currentIndex + 1) % patrolPoints.Count;
                 currentPatrolPointTarget = patrolPoints[currentIndex];
             }
 
             Vector3 direction = (currentPatrolPointTarget.position - transform.position).normalized;
-            
+
             RaycastHit hit;
             if (Physics.Raycast(transform.position, direction, out hit, 1.5f))
             {
@@ -50,7 +87,7 @@ public class GuardianSimplePatrol : MonoBehaviour
                     return;
                 }
             }
-            
+
             characterController.Move(direction * Time.deltaTime);
         }
     }
